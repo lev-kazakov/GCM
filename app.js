857c4486ba8d642f438099c283e5396e9e55447c
lev-kazakov <lev.kazakov@gmail.com>
2014-05-31 21:33:16 +0300

    init

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -27,8 +25,6 @@ if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
