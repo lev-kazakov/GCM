cdf0fca9dce4b771dabf90fefea0aa989b5394b1
lev-kazakov <lev.kazakov@gmail.com>
2014-06-02 19:28:13 +0300

    init

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ http.createServer(app).listen(app.get('port'), function(){
 app.use(express.bodyParser());
 
 app.post('/', function(req, res){
-    setTimeout(function() {
-        SayHey(req.body.regid, req.body.name);
-    }, 10000); 
+    //setTimeout(function() {
+    SayHey(req.body.regid, req.body.name);
+    //}, 10000); 
 });
 
 //APA91bHCVTF_nDeo58PudKU_urBG6D-Sy7cv3lPbMMUe7MnYSUnSYvXH-GRzmkobja7rCMoJSsHjfw9tAZ932d7rOYj-CfUsBMFE1toOXI8_k1NEq653CB3AtAwRBy__aaullSYcT7SBJh9zMntoLbn-kZUV88kC5Hcubx5z0dmU_xX0M1WWw1Q
--- batch.bat
+++ batch.bat
@@ -2,7 +2,6 @@ git add .
 git commit -m "init"
 git push -u origin master
 lev.kazakov@gmail.com
-levk0364061
 git push heroku master
 heroku ps:scale web=1
 heroku ps
