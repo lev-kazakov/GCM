99613da34a150715ef3a242a9077ca4996af9064
lev-kazakov <lev.kazakov@gmail.com>
2014-06-04 16:03:33 +0300

    init

--- app.js
+++ app.js
@@ -1,52 +1,33 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var http = require('http');
-var path = require('path');
 
 var app = express();
+var regids = [];
 
-// all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
 
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.use(express.bodyParser());
-
 app.post('/', function(req, res){
+    regids.push(req.body.regid);
+    var name = req.body.name.substring(0, req.body.name.indexOf('@'));
+    
     setTimeout(function() {
-        SayHey(req.body.regid, req.body.name);
+        notify(name);
     }, 5000); 
 });
 
-//APA91bHCVTF_nDeo58PudKU_urBG6D-Sy7cv3lPbMMUe7MnYSUnSYvXH-GRzmkobja7rCMoJSsHjfw9tAZ932d7rOYj-CfUsBMFE1toOXI8_k1NEq653CB3AtAwRBy__aaullSYcT7SBJh9zMntoLbn-kZUV88kC5Hcubx5z0dmU_xX0M1WWw1Q
 
-function SayHey(regid, name) { 
+function notify(regid, name) { 
     var post_data = JSON.stringify({
         "data": {
-            "title": "hey " + name + "!",
+            "title": name + " subscribed!\n everybody say welcome!!!",
             "message": "message"
         },
-        "registration_ids": [regid]
+        "registration_ids": regids
     });
     
     var post_options = {
